3ff7a518af8066ba837b9776032ada3a43ab9f6b
Vlad Kuznetsov] <vlad.kuzntosv@mail.ru>
2023-01-08 11:23:33 +0300

    add jest and storybook

---
+++ src/App/App.test.tsx
@@ -0,0 +1,9 @@
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import App from './App'
+
+test('Be in the document', () => {
+    render(<App />)
+
+    expect(screen.getByTestId('App')).toBeInTheDocument()
+})
